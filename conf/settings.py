71825173d835a833b8e05cc315c308b4692aeb32
Alex Fink <alexanderjfink@gmail.com>
2013-10-08 23:12:26 -0500

    Failed to commit the real settings.py… for whatever reason it wasn't in the doc at the time.

---
+++ conf/settings.py
@@ -0,0 +1,24 @@
+# sql_database						connection information for relational database
+# nosql_database					connection information for doc store
+# tmp_directory						directory to store temporary download files while extracting
+# delete_tmp_on_extract_complete	delete or save temporary files when extracting is finished?
+# drop_if_exists					if database or table already exists, should it be ignored or dropped?
+# debug								should debug information be displayed?
+
+conf = {
+	'sql_database': {
+		'type': 'mysql',
+		'username': 'specialuser',
+		'password': 'specialpass',
+		'hostname': 'localhost',
+	},
+
+	'nosql_database': {
+		'type': 'hadoop',
+	},
+
+	'tmp_directory': './tmp/',
+	'delete_tmp_on_extract_complete': True,
+	'drop_if_exists': True,
+	'debug': True,
+}
